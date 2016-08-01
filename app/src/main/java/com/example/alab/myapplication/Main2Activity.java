f498aca423af3089b5a001b57b1ddf7ce0e200b7
Alab <Alab@ad.tnt>
2016-08-01 16:59:27 +0800

    Initial commit

---
+++ app/src/main/java/com/example/alab/myapplication/Main2Activity.java
@@ -0,0 +1,16 @@
+package com.example.alab.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+//        Intent Intent_page2 = this.getIntent();
+    }
+}
